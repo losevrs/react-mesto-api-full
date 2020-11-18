f01053b6008864611bbfcf2a0151b02293c6966f
Лосев Родион Станиславович <@Lizin.local>
2020-11-18 14:20:26 +0300

    fix: адрес апи

--- backend/app.js
+++ backend/app.js
@@ -30,8 +30,8 @@ app.use(srvLog); // Логирование запросов к серверу
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
