b749b6e5b1253a67dac6bcb1cb019af5a31dd681
Лосев Родион Станиславович <@Lizin.local>
2020-11-18 13:56:15 +0300

    fix: адрес апи

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,6 @@ const { sendError } = require('./validation/errors');
 
 const { PORT = 3000 } = process.env;
 
-const { allowedCors } = require('./validation/allowedCORS');
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -31,10 +29,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(srvLog); // Логирование запросов к серверу
 
 app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
